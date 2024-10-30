6c82dbb4946de3e174af47727f481caf9dc844f9
Patrick_Foster <ezq9qu.virginia.edu>
2024-10-30 15:16:35 -0400

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BookLover',
+    version='0.1',
+    description='A demonstration package for homework M09',
+    url='https://github.com/pfost-bit/m09-demo',
+    author='Pat Foster',
+    author_email='ezq9qu@virginia.edu',
+    license='MIT',
+    packages=['booklover']
+)
